33b3b28ef19a068164f60f059385a08ad3bd2c26
nigelloch <37912573+nigelloch@users.noreply.github.com>
2021-12-23 18:23:59 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,115 @@
-var searchBtn = document.querySelector("#search-btn")
-var zipBtn = document.querySelector("#zip")
-console.log("buttonworks")
 
-var currentLocationEl = document.querySelector("#currentLocation")
+var googleApiKey = "AIzaSyDW9m-QeI0q_p6yQZ_vpMvfuuwxTOPBoOc"
 
-var apiKey = "AIzaSyDW9m-QeI0q_p6yQZ_vpMvfuuwxTOPBoOc"
+var seatGeekApi = 'MjQ5NjgyNzZ8MTYzOTcwODEzMS4yNTQzNDE0'
 
+var locationBtn = document.querySelector("#search-btn")
+
+    // get user lat/long
 var searchcurrentLocation = function(e) {
     e.preventDefault()
     console.log(e)
+    
+    navigator.geolocation.getCurrentPosition(function(pos){
+        console.log(pos.coords)
+             var lat=pos.coords.latitude
+             var long=pos.coords.longitude
+             console.log(lat,long)
+             getLocation(lat,long)
+            })
+        };
 
-    /*fetch("https://www.googleapis.com/geolocation/v1/geolocate?/client_id=" + apiKey)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            var state = document.querySelector("#state");
-                state.textContent = state 
-        })
-        https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
+    // user selects range and clicks button
+function getLocation(lat,long){
 
-        */
-        navigator.geolocation.getCurrentPosition(function(pos){
-            console.log(pos.coords)
-         var lat=pos.coords.latitude
-         var long=pos.coords.longitude
-         console.log(lat)
-         getCityName(lat,long)
-        })
+    // radius drop down menu
+    var radius = document.getElementById("radius").value
+    console.log(radius)
 
-}
-function getCityName(lat,long){
+    // #1 fetch user zip code using lat/long from navigator.geolocation
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${googleApiKey}`)
 
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${apiKey}`)            // ` for interprelation
+    .then(response => response.json())
+    .then(data => {
+        // pull down user zip code from blob
+        var zipCode = data.results[0].address_components[6].long_name
+
+    // #2 fetch seat geek venue/show information using the user zip code and radius
+     fetch("https://api.seatgeek.com/2/events?geoip=" + zipCode + "&range=" + radius + "mi&client_id=" + seatGeekApi + "&per_page=25")
             .then(response => response.json())
             .then(data => {
-                console.log(data.results[0].address_components[7].short_name)
-                getCityVenue(data.results[0].address_components[7].short_name)
+                console.log(data)
+
+                // begins building the html table body and iterates through seat geek blob - set to 25 results
+                document.getElementById("venue-table-body").innerHTML= ""
+                for(i=0; i<data.events.length; i++) {
+                    var tableRow = document.createElement("tr");
+                    
+                    // adjust date format to something more presentable 
+                    var unixDate = new Date(data.events[i].datetime_local).getTime()/1000
+                        var date = new Date(unixDate*1000)
+                        var month = date.getMonth();
+                        var day = date.getDate();
+                        var hours = date.getHours();
+                        var minutes = date.getMinutes();
+
+                        // convert from military time
+                        if (hours > 0 && hours <= 12) {
+                            hours;
+                          } else if (hours > 12) {
+                            hours= hours - 12;
+                          } else if (hours == 0) {
+                            hours = 12;
+                          }
+                          console.log(typeof minutes)
+                         if (minutes == 0){
+                             minutes = "00"
+                         }
+                         console.log(unixDate)
+
+                    // page time/date column
+                    var timeColumn = document.createElement("td");
+                    timeColumn.textContent = month + 1 + "/" + day + " " + hours + ":" + minutes + "pm"
+
+                    // page time/date column 
+                    var date = new Date(data.events[i].datetime_local).getTime()/1000
+                        console.log(date)
+
+                    // venue name column
+                    var venueColumn = document.createElement("td");
+                        venueColumn.textContent = data.events[i].venue.name
+
+                    // event name column
+                    var eventNameColumn = document.createElement("td");
+                        eventNameColumn.textContent = data.events[i].title
+
+                    // city/state/zip column
+                    var locationColumn = document.createElement("td");
+                        locationColumn.textContent = data.events[i].venue.extended_address
+
+                    // link to purchase tickets column
+                    var purchaseColumn = document.createElement("td");
+                         var anchorLink = document.createElement("a")
+                         anchorLink.setAttribute("href",data.events[i].url)
+                         anchorLink.textContent = "Buy Tickets!"
+                         purchaseColumn.appendChild(anchorLink)
+
+                    // creates rows for returned seat geek data
+                    tableRow.appendChild(timeColumn);
+                    tableRow.appendChild(venueColumn);
+                    tableRow.appendChild(eventNameColumn);
+                    tableRow.appendChild(locationColumn);
+                    tableRow.appendChild(purchaseColumn);
+                    document.getElementById("venue-table-body").appendChild(tableRow)
+                }
+
             })
+})
 }
-function getCityVenue(zipCode){
-// USe the zipcode to get venues with seatgeek
-// go to seatgeeks documentation page, ctrl f for zip or postal, then should show how to format a request using the zipcode. name, address and image 
-}
-zipBtn.addEventListener("click", searchcurrentLocation)
+
+locationBtn.addEventListener("click", searchcurrentLocation,getLocation)
+
+
+
+
+       
